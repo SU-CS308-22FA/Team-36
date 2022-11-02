40fa4348a4aac9882b954ce75c00fc6350277a57
saleh30002 <saleh@sabanciuniv.edu>
2022-11-02 16:06:58 +0300

    Update implemented

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-
+import Edit from "./pages/edit/Edit"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
 import { useContext } from "react";
-
+import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/AuthContext";
 
-
 function App() {
-  
-
-  const { currentUser } = useContext(AuthContext)
-
-  const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="/login" />;
-  };
-
   return (
-    <div className= "app">
+    <div className={"app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route path="login" element={<Login />} />
-            
-            <Route
-              index
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-            />
-            
+            <Route path="edit" element={<Edit />} />
           </Route>
         </Routes>
       </BrowserRouter>
