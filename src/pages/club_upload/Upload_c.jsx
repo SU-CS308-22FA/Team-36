26b8e08ba7e025f8adcf54d8e0c94708d5085519
Omar Mirza <omirza@sabanciuniv.edu>
2022-11-20 14:19:11 +0300

    uploading and deadline

--- src/pages/club_upload/Upload_c.jsx
+++ src/pages/club_upload/Upload_c.jsx
@@ -3,19 +3,25 @@ import Navbar from "../../components/navbar/Navbar"
 import document from './document.png'
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase";
+import { useEffect, useState } from "react";
+import moment from "moment/moment";
 
 const Upload_c = () => {
 
-
-const docRef = doc(db, "DocSubDeadlines", "TheYearlyDeadline");
-const docSnap = getDoc(docRef);
-
-if (docSnap.exists()) {
-  console.log("Document data:", docSnap.data());
-} else {
-  // doc.data() will be undefined in this case
-  console.log("No such document!");
-}
+const [ date, setDate ] = useState();
+    
+useEffect(async () => {
+    const docRef = doc(db, "DocSubDeadlines", "TheYearlyDeadline");
+    let docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        docSnap = new Date(docSnap.data().DateTime)
+        setDate(docSnap)
+        console.log("Document data:", date);
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}, []);
 
 
     return (
@@ -23,7 +29,7 @@ if (docSnap.exists()) {
             <Navbar />
             <div className="center">
                 <div className="container">
-                    <h2>Deadline: {}</h2>
+                    <h2>Deadline:</h2><span>{moment(date).format('DD-MM-YYYY')}</span>
                     <div className="block">
                         <h1>Upload Document</h1>
                         <img src={document}></img>
